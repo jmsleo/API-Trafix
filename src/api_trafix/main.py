9f430751d7c14593ca80a00dba4b1c27e9b37b49
2ybuser <andayudhobaz@gmail.com>
2026-08-11 00:38:17 +0700

    chore: resolve merge conflicts with development

--- src/api_trafix/main.py
+++ src/api_trafix/main.py
@@ -14,7 +14,7 @@ from api_trafix.core.middleware import RequestBodyLimitMiddleware, SecurityHeade
 from api_trafix.routes import member, shift, vehicle_type
 from api_trafix.routes.auth import router as auth_router
 from api_trafix.routes.users import router as users_router
-from api_trafix.routes import member, shift, vehicle_type, parking_rate, users, finance_dashboard, finance_reports
+from api_trafix.routes import member, shift, vehicle_type, parking_rate, users, finance_dashboard, finance_reports, operator_shift_assignment, operator_session
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -87,6 +87,8 @@ app.include_router(parking_rate.router)
 app.include_router(users.router)
 app.include_router(finance_dashboard.router)
 app.include_router(finance_reports.router)
+app.include_router(operator_session.router)
+app.include_router(operator_shift_assignment.router)
 
 @app.get("/")
 async def root():
