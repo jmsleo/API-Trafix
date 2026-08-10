8ee4a8c82a07cec4db8a3302c6a9d01911b27573
nzsnyn <672023013@student.uksw.edu>
2026-08-10 23:11:51 +0700

    feat(member): validate Indonesian phone number format

--- src/api_trafix/schemas/common.py
+++ src/api_trafix/schemas/common.py
@@ -1,6 +1,7 @@
-from typing import Annotated
+import re
+from typing import Annotated, Any
 
-from pydantic import EmailStr, Field, StringConstraints
+from pydantic import BeforeValidator, EmailStr, Field, StringConstraints
 
 NonNegativeInt = Annotated[int, Field(ge=0)]
 PositiveInt = Annotated[int, Field(ge=1)]
@@ -9,7 +10,24 @@ Name = Annotated[str, StringConstraints(min_length=1, max_length=100, strip_whit
 ShortName = Annotated[str, StringConstraints(min_length=1, max_length=50, strip_whitespace=True)]
 Code = Annotated[str, StringConstraints(min_length=1, max_length=20, strip_whitespace=True)]
 Email = Annotated[EmailStr, StringConstraints(strip_whitespace=True)]
-PhoneNumber = Annotated[str, StringConstraints(min_length=7, max_length=20, pattern=r"^\+?[0-9 ()-]+$")]
+
+
+def _normalize_phone(value: Any) -> Any:
+    if value is None:
+        return value
+    return re.sub(r"[\s()\-.]", "", str(value).strip())
+
+
+PhoneNumber = Annotated[
+    str,
+    BeforeValidator(_normalize_phone),
+    StringConstraints(
+        min_length=9,
+        max_length=16,
+        strip_whitespace=True,
+        pattern=r"^(\+?62|0)[2-9][0-9]{7,12}$",
+    ),
+]
 
 TicketNumber = Annotated[str, StringConstraints(min_length=1, max_length=50, strip_whitespace=True)]
 PoliceNumber = Annotated[
